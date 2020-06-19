08fe0e26fdabfa49cfb0e65adb8eeb1928218e0b
Candor-Dennis <candordennis74@gmail.com>
2020-06-19 15:39:29 +0000

    first commit

---
+++ Res/assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){ 
+	new WOW().init();
+
+
+});
+
+function go(){
+		alert('hello');
+	}
+
+	var x = 123;
+document.getElementById("demo").innerHTML =
+    x.toString() + "<br>" +
+   (123).toString() + "<br>" +
+   (100 + 23).toString();
+
+
